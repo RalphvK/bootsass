618e2d62be60f0d6b983d49f42b7c3b56946287e
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2019-01-24 15:37:06 +0100

    add minified scss

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,32 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
-// sass
-gulp.task('sass', function () {
-  return gulp.src('./scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'));
+var path = {
+    scss: './scss/style.scss',
+    css: './css'
+};
+
+// scss
+gulp.task('scss', function () {
+    return gulp.src(path.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(path.css))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.css));
+});
+gulp.task('scss-minify', function () {
+    return gulp.src(path.scss)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(path.css));
 });
 
 // concat
-gulp.task('concat_js', function() {
+gulp.task('concat_js', function () {
     // get file index 
     var includes = JSON.parse(fs.readFileSync('./js/index.json')).includes;
     //An array of files is required for the correct order of contact
@@ -22,18 +38,20 @@ gulp.task('concat_js', function() {
         .pipe(concat('./scripts.js'))
         .pipe(gulp.dest('./js/'))
         .pipe(minify({
-            ext:{
-                src:'',
-                min:'.min.js'
+            ext: {
+                src: '',
+                min: '.min.js'
             },
-            noSource: true}))
+            noSource: true
+        }))
         .pipe(gulp.dest('./js/'));
 });
 
 // watch
 gulp.task('watch', function () {
-  gulp.watch('./js/partials/**/*.js', gulp.series('concat_js'));
-  gulp.watch('./js/index.json', gulp.series('concat_js'));
+    gulp.watch('./js/partials/**/*.js', gulp.series('concat_js'));
+    gulp.watch('./js/index.json', gulp.series('concat_js'));
+    gulp.watch('./scss/**/*.scss', gulp.series('scss', 'scss-minify'));
 });
 
-gulp.task('default', gulp.series('watch', 'concat_js'));
+gulp.task('default', gulp.series('watch', 'concat_js', 'scss', 'scss-minify'));
